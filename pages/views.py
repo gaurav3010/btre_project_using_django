e469fbac003ca9dc524b5b281d492a10b3f1f924
gaurav <gauravusadadiya1998@gmail.com>
2018-12-14 22:36:49 +0530

    Create project and pages app, Add Template and Static files, Create Listings and Realtors apps, Complete static files, media files, models, Complete Data from Listing model, Complete Pagination

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Listing
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings':listings
+    }
+    return render(request, 'pages/index.html', context)
+
+def about(request):
+    return render(request, 'pages/about.html')
