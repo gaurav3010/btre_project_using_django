a41d342d171fa6e58e822afe0992192e1b089796
gaurav <gauravusadadiya1998@gmail.com>
2018-12-15 14:27:06 +0530

    Complete Search view

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import state_choices, bedroom_choices, price_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -25,4 +26,9 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices
+    }
+    return render(request, 'listings/search.html', context)
