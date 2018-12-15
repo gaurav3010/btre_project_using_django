f2661224df67c7afeaf0edd86abd687f34191da6
gaurav <gauravusadadiya1998@gmail.com>
2018-12-15 19:16:51 +0530

    Complete Register Section

---
+++ accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'accounts/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        # Get Form Values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        # Check if Password match
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email use by other user')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, 
+                                                    first_name=first_name, last_name=last_name) 
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password must be match') 
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def logout(request):
+    return redirect('index.html')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- btre/urls.py
+++ btre/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
