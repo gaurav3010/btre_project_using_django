62c88ab12b69074ec02f48d9a22a497abae1530a
gaurav <gauravusadadiya1998@gmail.com>
2018-12-15 15:19:25 +0530

    Complete Search

--- listings/views.py
+++ listings/views.py
@@ -26,9 +26,43 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    query_list = Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+    
+    #city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+    
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+    
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)
+
     context = {
         'state_choices':state_choices,
         'bedroom_choices':bedroom_choices,
-        'price_choices':price_choices
+        'price_choices':price_choices,
+        'listings':query_list,
+        'values':request.GET
     }
     return render(request, 'listings/search.html', context)
