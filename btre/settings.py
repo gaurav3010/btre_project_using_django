ef0878d339517160fd565cd0c1f948ff6964a33b
gaurav <gauravusadadiya1998@gmail.com>
2018-12-19 21:30:14 +0530

    Complete Send Email Section

--- btre/settings.py
+++ btre/settings.py
@@ -138,3 +138,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gauravusadadiya1998@gmail.com'
+EMAIL_HOST_PASSWORD = 'gaurav3010'
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 def contact(request):
     if request.method == 'POST':
@@ -19,13 +20,31 @@ def contact(request):
                 
                 if has_contacted:
                         messages.error(request, 'You have already made an inquiry for this listing')
+                contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, 
+                            message=message, user_id=user_id)
+                contact.save()
+                send_mail(
+                        'Property Listing Inquiry',
+                        'There has been an inquiry for ' + listing + '. Sign into the admin for more info',
+                        'gauravusadadiya1998@gmail.com',
+                        [realtor_email, 'gauravusadadiya1998@gmail.com']
+                )
+                messages.success(request, 'Your request has been submited')
                 return redirect('/listings/'+listing_id)
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, 
                             message=message, user_id=user_id)
 
         contact.save()
+
+        send_mail(
+                'Property Listing Inquiry',
+                'There has been an inquiry for ' + listing + '. Sign into the admin for more info',
+                 'gauravusadadiya1998@gmail.com',
+                 [realtor_email, 'gauravusadadiya1998@gmail.com']
+        )
+
         messages.success(request, 'Your request has been submited')
 
-        return redirect('/listing/'+listing_id)
+        return redirect('/listings/'+listing_id)
         
